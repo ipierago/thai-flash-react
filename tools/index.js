a20d7276b73c8b123e0bde4b801cc28199ac92c7
Ian Pieragostini <ian.pieragostini@gmail.com>
2022-12-09 12:07:11 +0700

    9 Dec

--- tools/index.js
+++ tools/index.js
@@ -25,16 +25,16 @@ async function main() {
     'world builder',
   ]);
   */
-  //await addNewTerms('gitignore/new-terms.txt', ['kruu bank', '7 dec 2022']);
+  //await addNewTerms('gitignore/new-terms.txt', ['kruu bank', '9 dec 2022']);
   //await mergeQuizlet();
   //await ensureAllMP3();
   /*
   await anki.exportListenDeck(
-    ['kruu bank', '7 dec 2022'],
+    ['kruu bank', '9 dec 2022'],
     'gitignore/anki.listen.txt'
   );
   await anki.exportSpeakDeck(
-    ['kruu bank', '7 dec 2022'],
+    ['kruu bank', '9 dec 2022'],
     'gitignore/anki.speak.txt'
   );
   */
